4044364c6a01298a721870304ee69e45fd0db1e2
kkahlua <csw93083@naver.com>
2024-05-03 15:33:51 +0900

    [Feat] Splash

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
+
 import React, {useEffect} from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './src/Router.tsx';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
   useEffect(() => {
-    if (__DEV__) {
-      import('./ReactotronConfig').then(() =>
-        console.log('Reactotron Configured'),
-      );
+  if (__DEV__) {
+        import('./ReactotronConfig').then(() =>
+          console.log('Reactotron Configured'),
+        );
+
+      setTimeout(() => {
+            SplashScreen.hide();
+          }, 1000);
     }
+
   }, []); // Reactotron 연결
 
   return (
--- android/app/src/main/java/com/nengboo_app/MainActivity.kt
+++ android/app/src/main/java/com/nengboo_app/MainActivity.kt
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen
 
 class MainActivity : ReactActivity() {
 
@@ -11,6 +12,9 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+  init {
+    SplashScreen.show(this)
+  }
   override fun getMainComponentName(): String = "nengboo_app"
 
   /**
