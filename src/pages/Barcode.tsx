35e7709794669fce2ed956394e20e3eedca7f7e9
Ieejueun <zziya2006@gmail.com>
2024-05-16 22:53:16 +0900

    [Fix] 이미지 처리 수정

--- src/pages/Barcode.tsx
+++ src/pages/Barcode.tsx
@@ -1,11 +1,12 @@
 import React, {useState, useEffect, useCallback} from 'react';
-import {SafeAreaView, Alert} from 'react-native';
+import {SafeAreaView, Platform} from 'react-native';
 import {useFocusEffect} from '@react-navigation/native';
 import {Code, CodeScannerFrame, PhotoFile} from 'react-native-vision-camera';
 import BarcodeScanner from '../components/BarcodeScanner.tsx';
 import useCameraPermissions from '../hooks/useCameraPermission.tsx';
 import {RESULTS} from 'react-native-permissions';
 import {supabase} from '../utils/supabase.tsx';
+import RNFetchBlob from 'rn-fetch-blob';
 import type {StackNavigationProp} from '@react-navigation/stack';
 import type {RootStackParamList} from '../Router';
 
@@ -106,51 +107,65 @@ const Barcode: React.FC<BarcodeProps> = ({navigation}) => {
     }
   };
 
+  // const getFileUri = (uri: string) => {
+  //   return Platform.OS === 'android' ? uri : uri.replace('file://', '');
+  // };
+
   const handleImageCaptured = async (
     photo: PhotoFile,
     barcodeData?: string,
   ) => {
     try {
-      const fileName = `public/product_${Date.now()}.jpg`;
-      const {data: imageData, error: imageError} = await supabase.storage
-        .from('images')
-        .upload(fileName, photo, {
-          contentType: 'image/jpeg',
-        });
+      const fileName = `public/product_${Date.now()}.jpeg`;
+      const fileUri = photo.path;
+      const response = await RNFetchBlob.fetch(
+        'POST',
+        `https://whrmaertzkkanlgksexz.supabase.co/storage/v1/object/images/${fileName}`,
+        {
+          'Content-Type': 'multipart/form-data',
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsImtpZCI6IjBNNWdPNnZBcGM4eGxvc1kiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzE1ODY5ODE5LCJpYXQiOjE3MTU4NjYyMTksImlzcyI6Imh0dHBzOi8vd2hybWFlcnR6a2thbmxna3NleHouc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6IjYzOTY1N2Y4LTdlYTQtNGNkNC1iMTI2LWNhZjdhOWUxNTRkYiIsImVtYWlsIjoienppeWEyMDA2QGdtYWlsLmNvbSIsInBob25lIjoiIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoia2FrYW8iLCJwcm92aWRlcnMiOlsia2FrYW8iLCJnb29nbGUiXX0sInVzZXJfbWV0YWRhdGEiOnsiYXZhdGFyX3VybCI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS9hL0FDZzhvY0wzUGdyVDQtTUdTWlpYUWFxUU5jdzdkZFZzRWZtVE1rSHFObklCUmxFOXVUUVlnWmtJPXM5Ni1jIiwiZW1haWwiOiJ6eml5YTIwMDZAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZ1bGxfbmFtZSI6IkFnIiwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50cy5nb29nbGUuY29tIiwibmFtZSI6IkFnIiwicGhvbmVfdmVyaWZpZWQiOmZhbHNlLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jTDNQZ3JUNC1NR1NaWlhRYXFRTmN3N2RkVnNFZm1UTWtIcU5uSUJSbEU5dVRRWWdaa0k9czk2LWMiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiLsnbTsp4DsnYAiLCJwcm92aWRlcl9pZCI6IjExMDQ1MDU2MzM0NjQ4NzU3NTA1NCIsInN1YiI6IjExMDQ1MDU2MzM0NjQ4NzU3NTA1NCIsInVzZXJfbmFtZSI6IuydtOyngOydgCJ9LCJyb2xlIjoiYXV0aGVudGljYXRlZCIsImFhbCI6ImFhbDEiLCJhbXIiOlt7Im1ldGhvZCI6Im9hdXRoIiwidGltZXN0YW1wIjoxNzE1ODYyMTE0fV0sInNlc3Npb25faWQiOiJjNzc3NmVlMS00ZTM2LTRjMzgtYjczYi1mZDYxOGJlMzllNjIiLCJpc19hbm9ueW1vdXMiOmZhbHNlfQ.PyKl5RGsdB7OH24-6Ur8wDAdUa3E3HhvDDe7N1Qgygc',
+        },
+        [
+          {
+            name: 'image',
+            filename: fileName,
+            type: 'image/jpeg',
+            data: RNFetchBlob.wrap(fileUri),
+          },
+        ],
+      );
 
-      if (imageError) {
-        console.error('이미지 업로드 에러:', imageError.message);
+      if (response.respInfo.status !== 200) {
+        console.error('이미지 업로드 실패', response.data);
+        return;
       }
 
       const publicUrl = await supabase.storage
         .from('images')
-        .getPublicUrl(`${imageData!.path}`);
-
-      const url = publicUrl.data.publicUrl;
+        .getPublicUrl(fileName);
+      const product_image = publicUrl.data.publicUrl;
 
       if (barcodeData) {
         await supabase
           .from('products')
-          .update({product_image: url})
+          .update({product_image: product_image})
           .eq('barcode', barcodeData);
-
         setPhotoCaptured(true);
       } else {
-        const productData = {
-          product_image: url,
+        const insertData = {
+          product_image: product_image,
           product_cookable: 'ingredient',
         };
 
         const {data: insertedData, error: insertError} = await supabase
           .from('products')
-          .insert([productData])
-          .select('product_id');
+          .insert([insertData])
 
         if (insertError) {
           throw new Error('제품 정보 삽입 실패: ' + insertError.message);
         }
 
-        setProductId(insertedData[0].product_id);
         setPhotoCaptured(true);
       }
     } catch (error) {
